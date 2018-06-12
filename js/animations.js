fc44fa34e59cfd646e45dcadd779da65f307656b
Michał Irzykowski <m.irzykowski@gmail.com>
2018-06-12 21:25:51 +0200

    add fontello file for icons and small changes in animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	$('.page-up').click(function() {
-		$("html, body").animate({ scrollTop: 0 }, 600);
-	});
-
 	$('.confirmation').click(function() {
 		$(this).fadeOut('slow');
 	});
