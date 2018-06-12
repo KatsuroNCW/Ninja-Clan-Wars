57fdc98778966aad4d9c75fcf49b571d38893091
Michał Irzykowski <m.irzykowski@gmail.com>
2018-06-12 21:15:29 +0200

    mobileMenu module refactor

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,28 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-	(function() {
-		const mobileMenu = document.querySelector('.mobile-nav__menu');
-		const landSwitcher = document.querySelector('.land-switcher');
-		const landSwitcherButton = mobileMenu.querySelector('.mobile-nav__item--switcher');
-		const mainNav = document.querySelector('.main-navigation');
-		const menuButton = mobileMenu.querySelector('.mobile-nav__item--menu');
-		const submenuButtons = mainNav.querySelectorAll('.has-submenu > a');
-		console.log(submenuButtons);
+const mobileMenu = (function() {
+	const menuContainer = document.querySelector('.mobile-nav');
+	const menuButtons = menuContainer.querySelectorAll('.mobile-nav__item a');
+	const landSwitcher = document.querySelector('.land-switcher');
+	const mainNav = document.querySelector('.main-navigation');
+	const submenuButtons = mainNav.querySelectorAll('.has-submenu > a');
+	const pageUrl = new String(document.URL);
+	const reg = /forum\.php/;
+	let lastScrollPosition = 0;
 
-		landSwitcherButton.addEventListener('click', function(e) {
-			e.preventDefault();
-			landSwitcher.classList.toggle('land-switcher--active')
+	const overlay = document.createElement("div");
+	overlay.classList.add("nav-overlay");
+	const body = document.querySelector('body');
+
+	const init = function() {
+		[].forEach.call(menuButtons, function(button) {
+			if(!reg.test(pageUrl) && button.dataset.click === 'land_switcher') {
+				button.parentElement.remove();
+			}
+			if(button.dataset.click !== undefined) {
+				button.addEventListener('click', function(e) {
+					e.preventDefault();
+					if(button.dataset.click === 'menu') {
+						body.appendChild(overlay);
+						mainNav.classList.add('main-navigation--active');
+					} else if(button.dataset.click === 'land_switcher') {
+						landSwitcher.classList.toggle('land-switcher--active');
+					}
+				});
+			}
 		});
 
-		menuButton.addEventListener('click', function(e) {
-			e.preventDefault();
-			mainNav.classList.toggle('main-navigation--active');
-		});
-
-		for (var i=0; i<submenuButtons.length; i++) {
-			submenuButtons[i].addEventListener('click', function(e) {
+		[].forEach.call(submenuButtons, function(button) {
+			button.addEventListener('click', function(e) {
 				e.preventDefault();
-				this.parentElement.querySelector('.submenu').classList.toggle('submenu--active');
 			});
-		}
-	}());
-}());
+		});
+
+		overlay.addEventListener('click', function() {
+			overlay.remove();
+			mainNav.classList.remove('main-navigation--active');
+		});
+	};
+	const display = function() {
+		(window.scrollY > lastScrollPosition) ? menuContainer.classList.add('mobile-nav--hidden') : menuContainer.classList.remove('mobile-nav--hidden');
+		lastScrollPosition = window.scrollY;
+	};
+
+	return {
+		init : init,
+		display : display
+	}
+})();
+
+document.addEventListener("DOMContentLoaded", function() {
+    mobileMenu.init();
+
+	window.addEventListener('scroll', function() {
+		mobileMenu.display();
+	});
+});
